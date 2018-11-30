580c5a0ae814d797f655e413400eda44b6fc43a5
ToddYang <yanggt97@gmail.com>
2018-11-30 13:42:41 -0500

    updated the console message for MDR
    Adding Team website
    
    Upload ArduCopter folder
    
    test

---
+++ src/Driver/cmake-build-debug/CMakeFiles/Driver.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Driver.dir/main.cpp.o"
+  "CMakeFiles/Driver.dir/gpio.cpp.o"
+  "CMakeFiles/Driver.dir/Mux.cpp.o"
+  "Driver.pdb"
+  "Driver.exe"
+  "libDriver.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Driver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
