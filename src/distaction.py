a2de4ac3eaa04569ba67d47251831dd5e11d0a4a
kaiqiann <kaiqian@umass.edu>
2019-04-14 15:40:44 -0400

    kai_again^4

--- src/distaction.py
+++ src/distaction.py
@@ -4,15 +4,23 @@ from pymavlink import mavutil
 import time
 from turtle import Turtle, Screen
 import array as arr
+import sys
 #import winsound
 
 vehicle = connect('127.0.0.1:14551', wait_ready=True)
 watchdist = input("Set the WATCH distance: ")
 alertdist = input("Set the ALERT distance: ")
 index = 0
-flag =0
+flag = 0
 indexflag = 0
-#arr.array('i', [0,0,0,0,0,0,0,0])
+status_flag_enable = True
+statusflag = False
+#sensorbuffer = arr.array('I', [sys.maxint,sys.maxint,sys.maxint,sys.maxint,sys.maxint,sys.maxint,sys.maxint,sys.maxint])
+
+sensorbuffer = []
+for i in range(8):
+    sensorbuffer.append(sys.maxint)
+
 position0, position1, posiiton2, posiiton3, position4, position5, position6, position7 = 0, 0, 0, 0, 0, 0, 0, 0
 previousmode = ''
 
@@ -27,10 +35,19 @@ def axis(turtle, distance, tick):
     for _ in range(0, distance, tick):
         turtle.backward(tick)
 
+
+def checkmode():
+    statusflag = False
+    for i in range(8):
+        if sensorbuffer[i] <=alertdist:
+            statusflag = True
+
+
 def my_method(self, name, msg):
     #x = math.cos(math.radians(22.5))*msg.current_distance
     #alert = alertdist
     print(msg.current_distance)
+    sensorbuffer[msg.orientation]= msg.current_distance
     #print(x)
     global flag
     global indexflag
@@ -77,19 +94,27 @@ def my_method(self, name, msg):
 
         
     #update dots in graph
-    global index
-    if index > 7:
-        index = 0
-
     #global position0, position1, posiiton2, posiiton3, position4, position5, position6, position7
     global position0
-    if msg.current_distance <= watchdist:
+    checkmode()
+    if statusflag == True and status_flag_enable == True:
+        vehicle.mode = VehicleMode("BRAKE")
+        if airspeed<=0.25*1:
+            vehicle.mode = VehicleMode("ALTHOLD")
+            status_flag_enable = False
+            print("-----------")
+            print("now it's BACK in Althold mode")
+            print("-----------")
+
+
+    if sensorbuffer[msg.orientation] <= watchdist:
         #msg.current_distance > alertdist and 
         #winsound.Beep(600, 1000)
-        print("It's sensor: ", repr(index))
+        print("It's sensor:",repr(index))
         turtle.penup()
-        if index ==0:
+        if msg.orientation == 0:
             #position0 = turtle.posiion()
+
             if indexflag == 0:
                 indexflag =1
             else:
@@ -100,7 +125,7 @@ def my_method(self, name, msg):
             turtle.dot(10,"red")
             turtle.penup()
 
-        elif index ==1:
+        elif msg.orientation ==1:
             #position1 = turtle.position()
             if indexflag == 0:
                 indexflag =1
@@ -112,7 +137,7 @@ def my_method(self, name, msg):
             turtle.dot(10,"red")
             turtle.penup()
 
-        elif index ==2:
+        elif msg.orientation ==2:
             #position2 = turtle.position()
             if indexflag == 0:
                 indexflag = 1
@@ -124,7 +149,7 @@ def my_method(self, name, msg):
             turtle.dot(10,"red")
             turtle.penup()
 
-        elif index ==3:
+        elif msg.orientation ==3:
             #position3 = turtle.position()
             if indexflag == 0:
                 indexflag = 1
@@ -136,7 +161,7 @@ def my_method(self, name, msg):
             turtle.dot(10,"red")
             turtle.penup()
 
-        elif index ==4:
+        elif msg.orientation ==4:
             #position4 = turtle.position()
             if indexflag == 0:
                 indexflag = 1
@@ -148,7 +173,7 @@ def my_method(self, name, msg):
             turtle.dot(10,"red")
             turtle.penup()
 
-        elif index ==5:
+        elif msg.orientation ==5:
             #position5 = turtle.position()
             if indexflag == 0:
                 indexflag = 1
@@ -160,7 +185,7 @@ def my_method(self, name, msg):
             turtle.dot(10,"red")
             turtle.penup()
 
-        elif index ==6: 
+        elif msg.orientation ==6: 
             #position6 = turtle.position()
             if indexflag == 0:
                 indexflag = 1
@@ -172,7 +197,7 @@ def my_method(self, name, msg):
             turtle.dot(10,"red")
             turtle.penup()
 
-        elif index ==7:
+        elif msg.orientation ==7:
             #position7 = turtle.position()
             if indexflag == 0:
                 indexflag = 1
@@ -185,18 +210,11 @@ def my_method(self, name, msg):
             turtle.penup()
 
         print("-----------")
-        #vehicle.mode = VehicleMode("BRAKE")
+        vehicle.mode = VehicleMode("BRAKE")
         #previousmode = vehicle.mode.name
         print("it's within the alert distance, control override!")
         print("now it's in BRAKE mode")
-        print("-----------")
-    else:
-        print("-----------")
-        #vehicle.mode = VehicleMode(previousmode)
-        print("now it's BACK in GUIDED mode")
-        print("-----------")
-
-    index+=1
+        print("-----------")        
 
 
 vehicle.add_message_listener('DISTANCE_SENSOR',my_method)
